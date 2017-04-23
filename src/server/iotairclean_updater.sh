23ed6f14e80d682d7b5c84faace13689a9e6c62f
Mike Ulm <michael.ulm@mea-it.com>
2017-04-23 12:47:10 +0200

    + new auto updater

---
+++ src/server/iotairclean_updater.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm iotairclean_subscriber.py
+wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/iotairclean_subscriber.py
+chmod +x iotairclean_subscriber.py
+
+echo "IoT AirClean Update finished!";
