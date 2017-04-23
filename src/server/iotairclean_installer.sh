d6b68ed2e21d7c61fc47ba77a803ebd724baa8b7
Mike Ulm <michael.ulm@mea-it.com>
2017-04-23 15:43:29 +0200

    + nginx config with php

--- src/server/iotairclean_installer.sh
+++ src/server/iotairclean_installer.sh
@@ -2,6 +2,14 @@
 sudo apt-get -y install nginx
 sudo /etc/init.d/nginx start
 
+# prepare / modify standard html intstallation
+sudo mv /var/www/html/ /var/www/html_orig/
+sudo ln -s /var/www/html /usr/share/nginx/html
+cd /etc/nginx/sites-available
+sudo mv default default_orig
+wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/config/default
+
+
 # install php 
 sudo apt-get -y install php5-fpm
 sudo /etc/init.d/nginx reload
@@ -19,6 +27,7 @@ sudo /etc/init.d/php5-fpm restart
 sudo apt-get -y install ntpdate
 
 # download current update script
+cd /home/pi
 wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/backend/iotairclean_update.sh
 chmod +x iotairclean_update.sh
 ./iotairclean_update.sh
