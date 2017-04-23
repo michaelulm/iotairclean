cb2517ffe98551d350d4c8b94645cf27f1ab8f36
Mike Ulm <michael.ulm@mea-it.com>
2017-04-23 12:58:49 +0200

    + add new structure element "ui" for future webui elements

--- src/server/iotairclean_updater.sh
+++ src/server/iotairclean_updater.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo "IoT AirClean Update started!";
+
+# Update Current Python Scripts to get Data
 rm iotairclean_subscriber.py
 wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/iotairclean_subscriber.py
 chmod +x iotairclean_subscriber.py
 
+echo "IoT AirClean Background Tasks updated!";
+
+# Update Current ui
+cd /usr/share/nginx/html
+rm iot_airclean_station.php
+wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/iot_airclean_station.php
+
+echo "IoT AirClean UI updated!"
+
+# everything should be finished
 echo "IoT AirClean Update finished!";
---
+++ src/server/ui/iot_airclean_station.php
@@ -0,0 +1,56 @@
+<?php
+echo '<h2>PHP + MongoDB Test</h2>';
+
+// some configs for easier customizing
+$stationname = "wohnzimmer@home";
+if(isset($_GET["stationname"))
+	$stationname = $_GET["stationname"];
+
+try
+{
+        $mongo = new Mongo(/*"localahost:27101"*/);
+
+        $iotairclean = $mongo->iotairclean;
+
+        $measurements = $iotairclean->measurements;
+
+        $ms = $measurements->find(array('station' => $stationname));
+
+        if ($ms->count() <1)
+        {
+            //$posts->insert(array('title' => 'Hello, MongoDB!')); // inserts new values
+			//$posts->insert(array('temperature' => 26.4, 'humidity' => 45.3, 'co2' => 877, 'station' => "michaelulm@home"));// inserts new values
+
+			// but currently nothing to do
+        }
+        else
+        {
+                echo "<p>" . $ms->count() . ' measurements found.' ;
+				$lastMeasurement = $measurements->find(array('station' => $stationname))->sort(array( 'measured' => -1))->limit(1);
+				$values = $lastMeasurement->getNext();
+//print_r($values);
+				echo "<br/>Temperature: " . $values["temperature"] . "C";
+				echo "<br/>Humidity: " . $values["humidity"] . "%";
+				echo "<br/>CO2: " . $values["co2"] . "ppm";
+				echo "<br/>measured: " . date('d.m.Y H:i:s', $values["measured"]->sec);
+
+                                echo "<br/>Location: " . $values["location"];
+                                echo "<br/>Room: " . $values["room"];
+
+				echo "</p>";
+                // foreach($it as $obj)
+                // {
+                        // echo "title: [" . $obj["title"] . "]<br />";
+                // }
+        }
+        $mongo->close();
+}
+catch(MongoConnectionException $e)
+{
+        die('Error in connection to MongoDB' . $e->getMessage());
+}
+catch(MongoException $e)
+{
+        die('Error:' . $e->getMessage());
+}
+
