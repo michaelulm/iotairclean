8b45781e379c0d361b4a0f0d71f90638985cb93a
Mike Ulm <michael.ulm@mea-it.services>
2017-07-15 20:21:29 +0200

    # another bugfix because of missing change directory

--- src/server/backend/iotairclean_updater.sh
+++ src/server/backend/iotairclean_updater.sh
@@ -10,15 +10,18 @@ chmod +x iotairclean_subscriber.py
 echo "IoT AirClean Background Tasks updated!";
 
 # Update Current ui
+
+# create update directories
 mkdir /usr/share/nginx/html_update/css
 mkdir /usr/share/nginx/html_update/fonts
 mkdir /usr/share/nginx/html_update/js
 # basic files
+cd /usr/share/nginx/html_update/
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/iotairclean_station.php
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/iotairclean_visualization.php
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/iotairclean_logo.png
 # js, css and fonts
-cd css
+cd /usr/share/nginx/html_update/css
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap.css
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap.css.map
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap.min.css
@@ -27,15 +30,13 @@ sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/se
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap-theme.css.map
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap-theme.min.css
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/css/bootstrap-theme.min.css.map
-cd ..
-cd fonts
+cd /usr/share/nginx/html_update/fonts
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/fonts/glyphicons-halflings-regular.eot
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/fonts/glyphicons-halflings-regular.svg
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/fonts/glyphicons-halflings-regular.ttf
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/fonts/glyphicons-halflings-regular.woff
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/fonts/glyphicons-halflings-regular.woff2
-cd ..
-cd js
+cd /usr/share/nginx/html_update/js
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/js/bootstrap.js
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/js/bootstrap.min.js
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/js/Chart.min.js
@@ -44,7 +45,12 @@ sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/se
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/js/mqttws31.min.js
 sudo wget https://raw.githubusercontent.com/michaelulm/iotairclean/master/src/server/ui/js/npm.js
 
+# copy updated files
 sudo cp -rf /usr/share/nginx/html_update/* /usr/share/nginx/html
+# set files owner to nginx user
+chown -R nginx:nginx /usr/share/nginx/html
+# remove all downloaded files
+rm -rf /usr/share/nginx/html_update/*
 
 echo "IoT AirClean UI updated!"
 
