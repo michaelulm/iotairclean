964f951116a0387d62b000be8c380f12ca822a6e
Mike Ulm <michael.ulm@mea-it.services>
2017-10-26 19:08:41 +0200

    + integrated RGB Led for Sensorbox only usage

--- src/logger/iot_data_logger_nano/iot_data_logger_nano.ino
+++ src/logger/iot_data_logger_nano/iot_data_logger_nano.ino
@@ -1,6 +1,6 @@
 ////////////  IoT AirClean Settings
-String room = "testraum2.1";
-String location = "teststation2";
+String room = "testraum3.2";
+String location = "teststation3";
 // TODO currently high amount of data => MongoDB will get an out of memory error otherwise, we have to find a solution
 int intervalMeasurement = 30000; // each half minute
 //int intervalMeasurement = 1000; // each second
@@ -38,9 +38,22 @@ float tmpT = 0.0;
 float tmpH = 0.0;
 int tmpCO2 = 0;
 
+// LED RGB Config
+int redpin   = 10; // select the pin for the red LED
+int greenpin = 7;  // select the pin for the green LED
+int bluepin  = 6;  // select the pin for the  blue LED
+
+
 //////////// START OF APPLICATION 
 void setup() {
   Serial.begin(57600);
+
+  // setup rgb pins for write out
+  pinMode(redpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  led_colortest();
+  led_blue();
   
   // start with DHT Sensor
   dht.begin();
@@ -84,6 +97,7 @@ void loop() {
     tmpT = t;
   }
 
+  led_white();
   // Read co2 value by ppm
   if (mySensor.measure()) {
     Serial.print("CO2 Concentration is ");
@@ -91,6 +105,7 @@ void loop() {
     Serial.println("ppm");
     tmpCO2 = mySensor.ppm;
   } else {
+    led_blink(3);
     Serial.println("Sensor communication error.");
   }
 
@@ -103,10 +118,81 @@ void loop() {
    // without RTC
    String data = "{\"t\": " + String(tmpT) + ", \"h\": " + String(tmpH) + ", \"co2\": " + String(tmpCO2) + ", \"station\": \"" + room +"@"+location+"\", \"location\": \"" +location+"\", \"room\": \"" + room +"\" }";
 
+  // visualize current air quality by diagram color schema
+  if(tmpCO2 < 450){
+    led_white();
+  } else if(tmpCO2 < 800){
+    led_green();
+  } else if(tmpCO2 < 1200){
+    led_yellow();
+  } else if(tmpCO2 < 1600){
+    led_orange();
+  }else{
+    led_red();
+  }
+  
   // print for debugging
   Serial.println(data);
 
   // transfer to Raspberry PI with XBee Communication
   xbee.print( data);
 }
+
+// needed led methods
+void led_green(){
+  led_switch(0,255,0);
+}
+void led_red(){
+  led_switch(255,0,0);
+}
+void led_orange(){
+  led_switch(50,50,0);
+}
+void led_yellow(){
+  led_switch(255,255,0);
+}
+void led_white(){
+  led_switch(255,255,255);
+}
+void led_off(){
+  led_switch(0,0,0);
+}
+void led_blue(){
+  led_switch(0,0,255);
+}
+
+// blink x times white - off - white - off ...
+void led_blink(int times){
+  for(int nr = 0; nr < times; nr++){
+    led_white();
+    delay(500);
+    led_off();
+    delay(500);
+  }
+}
+
+// switch to wanted RGB value
+void led_switch(int r, int g, int b){
+    analogWrite(redpin, r);
+    analogWrite(greenpin, g);
+    analogWrite(bluepin, b);
+}
+
+// color test for startup to take care RGB Led is working correctly
+void led_colortest(){
+
+  led_blink(2);
+  
+  led_green();
+  delay(1000);
+  led_yellow();
+  delay(1000);
+  led_orange();
+  delay(1000);
+  led_red();
+  delay(1000);
+  
+  led_blink(2);
+}
+
 
